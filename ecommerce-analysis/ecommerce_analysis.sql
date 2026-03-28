9782103b51950838ca8430218410dd4f487c9394
Raza Ali-75 <razaalian876@gmail.com>
2026-03-28 16:00:49 +0530

    ecommerce_analysis.sql

--- ecommerce-analysis/ecommerce_analysis.sql
+++ ecommerce-analysis/ecommerce_analysis.sql
@@ -1,1 +1,57 @@
+/* ==============================================================================
+  Revenue Analysis
+Calculate monthly revenue trend.
+Show MoM (Month-over-Month) growth percentage.
+============================================================================== */
+---- Monthly Revenue growth
+
+
+WITH monthly_revenue AS (  --creating common table expresion that live inside our query
+    SELECT 
+        MONTH(order_date) AS month,
+        SUM(total_amount) AS total_sales
+    FROM orders
+    GROUP BY MONTH(order_date)
+)
+
+SELECT *,
+       ROUND(
+            (total_sales - LAG(total_sales) OVER (ORDER BY month)) 
+            / LAG(total_sales) OVER (ORDER BY month) * 100
+       ,2) AS mom_growth_percentage--calculate percentage growth
+FROM monthly_revenue;
+
+/* ==============================================================================
+   Product Performance
+Find Top 3 products per category by revenue.
+Find products selling below their cost (loss-making).
+============================================================================== */
+
+select*
+from(
+  select
+  product_id,
+  category,
+  revenue,
+  rank() over(partition by category order by revenue desc)rank
+  from(
+    select
+    p.product_id,
+    p.category,
+    SUM(o.quantity*o.price_per_unit) as revenue
+    from products p
+    left join order_items o
+    on p.product_id=o.product_id
+    group by p.product_id,p.category)t)t
+where rank<=3 --filtering Top 3 products per category by revenue
+
+/* ==============================================================================
+   Customer Behavior
+Calculate Customer Lifetime Value (CLV).
+Find repeat customers.
+Rank customers by total spending.
+============================================================================== */
+
+
+
 
